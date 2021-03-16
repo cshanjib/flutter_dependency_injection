9203cc9b67366134a545ec2cdb3776112c8df189
Sanjib <sanjib.maharjan@debajyotigroup.com>
2021-03-16 19:51:33 +0545

    Added a demo quote page

--- lib/main.dart
+++ lib/main.dart
@@ -9,96 +9,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Dependency Injection Demo',
+      home: MyQuotePage(title: 'Dependency Injection Demo Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MyQuotePage extends StatefulWidget {
+  MyQuotePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyQuotePageState createState() => _MyQuotePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyQuotePageState extends State<MyQuotePage> {
+  final List _data = [
+    {
+      "tags": ["software engineering", "programming", "design", "marketing"],
+      "quote":
+          "Simplicity is a great virtue but it requires hard work to achieve it and education to appreciate it. And to make matters worse: complexity sells better.",
+      "lang": "en",
+      "author": "Edsger Wybe Dijkstra"
+    },
+    {
+      "tags": ["science", "short", "ux", "knowledge", "ui", "software"],
+      "quote":
+          "One consequence of reifying deep principles in an interface is that mastering the subject begins to coincide with mastering the interface.",
+      "lang": "en",
+      "author": "Michael Nielsen"
+    },
+    {
+      "tags": ["short", "effective altruism", "impact"],
+      "quote":
+          "If you can help 1000 people be 1% more effective, then that’s like having the impact of 10 people.",
+      "lang": "en",
+      "author": "Ben Todd"
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
+        body: Center(
+          child: ListView.builder(
+            itemCount: _data.length,
+            itemBuilder: (context, index) => _quoteItem(_data[index], index),
+          ),
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+
+  _quoteItem(item, index) {
+    return ListTile(
+      tileColor: index % 2 == 0 ? Colors.white : Colors.grey.withOpacity(0.1),
+      title: Text(
+        '"${item["quote"]}"',
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Text('- ${item["author"]}',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10)),
+      ),
     );
   }
 }
